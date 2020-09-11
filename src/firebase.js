d39461690183fab01e8d40bffc30109ea0de8c66
Dario Kurecki <dario.kurecki@gmail.com>
2020-09-11 10:57:30 +0200

    Init commit.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSs1C6tKJfVzsivX3QYYyl8fTDuqyhi0c",
+  authDomain: "clone-ed3f9.firebaseapp.com",
+  databaseURL: "https://clone-ed3f9.firebaseio.com",
+  projectId: "clone-ed3f9",
+  storageBucket: "clone-ed3f9.appspot.com",
+  messagingSenderId: "1014832556054",
+  appId: "1:1014832556054:web:e528001ec59a0da9432d37",
+  measurementId: "G-X6CPCBR3CD",
+};
